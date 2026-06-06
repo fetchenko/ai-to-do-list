d0d6b4a84b1669f2d9c6c9ceec92a48fa9a19e24
tanyalfedchneko@gmail.com <tanyalfedchenko@gmail.com>
2026-06-06 12:06:48 +0300

    add tanstack provider

---
+++ app/providers.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { makeQueryClient } from "@/lib/query-client";
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState<QueryClient>(() => makeQueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}

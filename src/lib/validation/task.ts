af514d8001e1d6c6d050d6600217530cae151eca
tanyalfedchneko@gmail.com <tanyalfedchenko@gmail.com>
2026-06-12 17:03:20 +0300

    update forms  & add validation for auth

---
+++ src/lib/validation/task.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const taskSchema = z.object({
+  title: z
+    .string()
+    .min(3, "Title must be at least 3 characters")
+    .max(100, "Title is too long"),
+
+  description: z
+    .string()
+    .max(1000, "Description is too long")
+    .optional()
+    .or(z.literal("")),
+});

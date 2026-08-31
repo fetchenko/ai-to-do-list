a2ec4bea5495534e98a1ea5b5c638ebd84786014
Tanya Fedchenko <30293847+fetchenko@users.noreply.github.com>
2026-08-31 15:30:33 +0300

    feat: add schema for API errors

---
+++ src/shared/errors/api-error.schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+import { ErrorCode } from '@/shared/errors/code';
+
+export const apiErrorSchema = z.object({
+  success: z.literal(false),
+  code: z.enum(ErrorCode),
+  status: z.number().int().min(400).max(599),
+  message: z.string(),
+  details: z.unknown().optional(),
+});
+
+export type ApiError = z.infer<typeof apiErrorSchema>;

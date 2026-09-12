7ba0b9670552333319128efe156b2049517a722c
tanyalfedchneko@gmail.com <tanyalfedchenko@gmail.com>
2026-09-12 16:26:48 +0300

    clean up styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 
 import { AppFooter } from '@/components/layout/app-footer';
 import { AppHeader } from '@/components/layout/app-header';
-import { Container } from '@/components/layout/container';
 import ContentSkeleton from '@/features/home/components/content-skeleton';
 import HomeContent from '@/features/home/components/home-content';
 
@@ -10,12 +9,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <AppHeader />
-
-      <Container className="flex flex-1 justify-center py-6 sm:py-10">
+      <div className="flex flex-1 justify-center">
         <Suspense fallback={<ContentSkeleton />}>
           <HomeContent />
         </Suspense>
-      </Container>
+      </div>
 
       <AppFooter />
     </main>
